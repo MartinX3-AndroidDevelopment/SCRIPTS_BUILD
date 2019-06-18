747e389cf9e121212a0c13aeb4e264cae222968e
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2019-06-18 23:48:44 +0200

    Add OmniROM
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SCRIPTS_BUILD
 Buildscripts for the projects
+
+## Content
+- ROM
+  - CUSTOM
+    - XPERIA_TAMA
+      - [OmniROM](ROM/CUSTOM/XPERIA_TAMA/OmniROM/README.md)
---
+++ ROM/CUSTOM/XPERIA_TAMA/OmniROM/build.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+function set_variables() {
+    echo "####SET VARIABLES START####"
+    build_cache=/media/martin/extLinux/developer/android/cache/omniROM #CustomROM out dir
+    build_out=/media/martin/extLinux/developer/android/out/omniROM
+    current_dir=$(pwd)
+    customROM_dir=/home/developer/android/omniROM90
+    echo "####SET VARIABLES END####"
+}
+
+function add_custom_hacks() {
+    echo "####CUSTOMROM HACKS ADDING START####"
+    cd ${customROM_dir}
+
+    # TODO: Workaround for the camera focus issue until it gets fixed in the camera driver
+    cd ${customROM_dir}/kernel/sony/msm
+    git fetch https://github.com/kholk/kernel 232r14-headless-sde && git cherry-pick d5c2a4926c7ba56a983588c2f354620d4f8dcdd8
+
+    # TODO: Needed for decryption support, until it got merged into omnirom
+    cd ${customROM_dir}/system/sepolicy
+    git fetch https://gerrit.omnirom.org/android_system_sepolicy refs/changes/19/33719/4 && git cherry-pick FETCH_HEAD
+    cd ${customROM_dir}/build/make
+    git fetch https://gerrit.omnirom.org/android_build refs/changes/20/33720/3 && git cherry-pick FETCH_HEAD
+    echo "####CUSTOMROM HACKS ADDING END####"
+}
+
+function build_omniROM() {
+    echo "####OMNI ROM BUILD START####"
+    cd ${customROM_dir}
+    source ${customROM_dir}/build/envsetup.sh
+
+    echo "####$1 Sim START####"
+    case "$1" in
+        "xz2_SS")
+            product_name=akari
+        ;;
+        "xz2c_SS")
+            product_name=apollo
+        ;;
+        "xz3_SS")
+            product_name=akatsuki
+        ;;
+        *)
+            echo "Unknown Option $1 in build_omniROM()"
+            exit 1 # die with error code 9999
+    esac
+
+    lunch omni_${product_name}-userdebug
+
+    make -j$((`nproc`-1)) installclean # Clean build while saving the buildcache.
+
+    make -j$((`nproc`-1)) update-api
+
+    make -j$((`nproc`-1))
+
+    cp ${build_cache}/target/product/${product_name}/*.img ${build_out}/$1/
+
+    rm ${build_out}/$1/ramdisk*
+    rm ${build_out}/$1/dtbo-arm64.img
+    echo "####$1 Sim END####"
+    echo "####OMNI ROM BUILD END####"
+}
+
+echo "IS THIS SHELL IN THE REPOSITORY? Or did you modify the current_dir variable?"
+echo "Don't forget to upgrade the TWRP minimal sources!"
+read -n1 -r -p "Press space to continue..."
+
+source ../../../../TOOLS/functions.sh
+
+functions_init
+
+set_variables
+
+functions_create_folders ${build_cache}
+functions_create_folders ${build_out}
+functions_create_folders ${build_out}/xz2_SS
+functions_create_folders ${build_out}/xz2c_SS
+functions_create_folders ${build_out}/xz3_SS
+
+functions_test_repo_up_to_date
+
+functions_clean_builds ${build_out}/xz2_SS
+functions_clean_builds ${build_out}/xz2c_SS
+functions_clean_builds ${build_out}/xz3_SS
+
+functions_update_customROM ${customROM_dir}
+
+add_custom_hacks
+
+build_omniROM xz2_SS
+build_omniROM xz2c_SS
+build_omniROM xz3_SS
+
+functions_compress_builds ${build_out}/xz2_SS omnirom_xz2_ss
+functions_compress_builds ${build_out}/xz2c_SS omnirom_xz2c_ss
+functions_compress_builds ${build_out}/xz3_SS omnirom_xz3_ss
+
+functions_clean_builds ${build_out}/xz2_SS
+functions_clean_builds ${build_out}/xz2c_SS
+functions_clean_builds ${build_out}/xz3_SS
+
+echo "Output ${build_out}"
+read -n1 -r -p "Press space to continue..."
+echo "Upload to androidfilehost.com !"
+read -n1 -r -p "Press space to continue..."
+echo "Upload to dhacke strato server !"
+read -n1 -r -p "Press space to continue..."
+
+exit 0
+
